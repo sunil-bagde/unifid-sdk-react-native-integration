73b722afad56755f44cabae1a40c1ca58a02ea71
Sunil Bagde <bagdesunil786@gmail.com>
2022-03-06 06:00:09 +0530

    wip

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
+  const sdkRef =  useRef(null);
+  let webView;
+  const handleSdkInit = () => {      const sdkKey =
+       "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzZGsiLCJqdGkiOiI0YWEyNGZlNS0yZThmLTRiNzUtOWRiYi00OTlmYjZkYmJhYjQiLCJpYXQiOjE2NDYzODM2NTl9.AmakuxSejgxaQfhDBS4PUvFORktmvHPvLhPHdrLAChQ";
+     const message =  {
+       sdkUrl: "SDK_URL",
+       sdkKey: sdkKey,
+       verifierID: "sasasas",
+       message: "Message",
+       notification: "Message",
+       customData: { name: "Name" }
+     };
+      webView.postMessage(JSON.stringify(
+        { message: "onInit", value: message },
+      ));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#fff' }}>
+      <WebView
+                 originWhitelist={['*']}
+                 source={{ uri: "http://192.168.0.100:3000/" }}
+                 style={styles.frame}
+                 javaScriptEnabled={true}
+                 onLoadEnd={handleSdkInit}
+                 onMessage={(event) => {
+                   const data = event.nativeEvent.data
+                   console.log("data", data);
+                 }}
+                 ref={el => webView = el}
+                 scalesPageToFit />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  frame: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 300,
+    width:300
   },
 });
