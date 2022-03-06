362197980bdef481df5576263159d1d0c26ea788
Sunil Bagde <bagdesunil786@gmail.com>
2022-03-06 06:06:43 +0530

    wip

--- App.js
+++ App.js
@@ -1,41 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useRef} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { WebView } from 'react-native-webview';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useRef } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
-  const sdkRef =  useRef(null);
+  const sdkRef = useRef(null);
   let webView;
-  const handleSdkInit = () => {      const sdkKey =
-       "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzZGsiLCJqdGkiOiI0YWEyNGZlNS0yZThmLTRiNzUtOWRiYi00OTlmYjZkYmJhYjQiLCJpYXQiOjE2NDYzODM2NTl9.AmakuxSejgxaQfhDBS4PUvFORktmvHPvLhPHdrLAChQ";
-     const message =  {
-       sdkUrl: "SDK_URL",
-       sdkKey: sdkKey,
-       verifierID: "sasasas",
-       message: "Message",
-       notification: "Message",
-       customData: { name: "Name" }
-     };
-      webView.postMessage(JSON.stringify(
-        { message: "onInit", value: message },
-      ));
-  }
+  const handleSdkInit = () => {
+    const sdkKey =
+      "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzZGsiLCJqdGkiOiI0YWEyNGZlNS0yZThmLTRiNzUtOWRiYi00OTlmYjZkYmJhYjQiLCJpYXQiOjE2NDYzODM2NTl9.AmakuxSejgxaQfhDBS4PUvFORktmvHPvLhPHdrLAChQ";
+    const message = {
+      sdkUrl: "SDK_URL",
+      sdkKey: sdkKey,
+      verifierID: "sasasas",
+      message: "Message",
+      notification: "Message",
+      customData: { name: "Name" },
+    };
+    webView.postMessage(JSON.stringify({ message: "onInit", value: message }));
+  };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#fff' }}>
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: "#fff" }}>
       <WebView
-                 originWhitelist={['*']}
-                 source={{ uri: "http://192.168.0.100:3000/" }}
-                 style={styles.frame}
-                 javaScriptEnabled={true}
-                 onLoadEnd={handleSdkInit}
-                 onMessage={(event) => {
-                   const data = event.nativeEvent.data
-                   console.log("data", data);
-                 }}
-                 ref={el => webView = el}
-                 scalesPageToFit />
+        originWhitelist={["*"]}
+        source={{ uri: "http://192.168.0.100:3000/" }}
+        style={styles.frame}
+        javaScriptEnabled={true}
+        onLoadEnd={handleSdkInit}
+        onMessage={(event) => {
+          const data = event.nativeEvent.data;
+          console.log("data", data);
+        }}
+        ref={(el) => (webView = el)}
+        scalesPageToFit
+      />
     </View>
   );
 }
@@ -43,8 +42,8 @@ export default function App() {
 const styles = StyleSheet.create({
   frame: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     height: 300,
-    width:300
+    width: 300,
   },
 });
